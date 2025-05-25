6fd9860daad110aba8d06c7b846bf025ed9cb7f6
JulianeMaran32 <julianemaran@gmail.com>
2025-05-24 22:07:01 -0300

    Even or odd > Check even or odd

--- java-basic-exercises/src/CheckEvenOrOdd.java
+++ java-basic-exercises/src/CheckEvenOrOdd.java
@@ -1,6 +1,21 @@
-public class CountToN {
+import java.util.Scanner;
+
+public class CheckEvenOrOdd {
 
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite um número inteiro: ");
+        int number = scanner.nextInt();
+
+        if (number % 2 == 0) {
+            System.out.println(number + " é par.");
+        } else {
+            System.out.println(number + " é ímpar.");
+        }
+
+        scanner.close();
 
     }
 
