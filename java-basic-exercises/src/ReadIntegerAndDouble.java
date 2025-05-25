72c2d1368f5875c0982ef6d0ac009cc98a5d801c
JulianeMaran32 <julianemaran@gmail.com>
2025-05-24 22:04:55 -0300

    Read Values > Read Integer And Double

--- java-basic-exercises/src/ReadIntegerAndDouble.java
+++ java-basic-exercises/src/ReadIntegerAndDouble.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class ReadIntegerAndDouble {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite um número inteiro: ");
+        int inteiro = scanner.nextInt();
+
+        System.out.print("Digite um número real (double): ");
+        double real = scanner.nextInt();
+
+        System.out.println("Número inteiro: " + inteiro);
+        System.out.println("Número real (double): " + real);
+
+        scanner.close();
+
+    }
+
 }
