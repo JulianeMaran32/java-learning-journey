1fc09ae1b3e18557e176bb9027d11dd1b5372bd3
JulianeMaran32 <julianemaran@gmail.com>
2025-05-24 22:05:49 -0300

    Count up to N > Count to N

--- java-basic-exercises/src/CountToN.java
+++ java-basic-exercises/src/CountToN.java
@@ -1,6 +1,17 @@
-public class ReadIntegerAndDouble {
+import java.util.Scanner;
+
+public class CountToN {
 
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite um valor inteiro N: ");
+        int n = scanner.nextInt();
+
+        for (int i = 0; i <= n; i++) {
+            System.out.println(i);
+        }
 
     }
 
